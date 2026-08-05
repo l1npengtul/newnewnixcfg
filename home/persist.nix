b3864d76b3eb0779b789f7c1e3916173a53213e6
l1npengtul <l1npengtul@protonmail.com>
2026-08-06 01:31:01 +0900

    temporarily remove stignores

--- home/persist.nix
+++ home/persist.nix
@@ -27,11 +27,11 @@
     ];
   };
 
-  home.file.".wine/.stignore".source = ./stignores/stignore-wine;
-  home.file."Documents/.stignore".source = ./stignores/stignore-std;
-  home.file."Music/.stignore".source = ./stignores/stignore-std;
-  home.file."Pictures/.stignore".source = ./stignores/stignore-std;
-  home.file."supernote/.stignore".source = ./stignores/stignore-std;
+  #home.file.".wine/.stignore".source = ./stignores/stignore-wine;
+  #home.file."Documents/.stignore".source = ./stignores/stignore-std;
+  #home.file."Music/.stignore".source = ./stignores/stignore-std;
+  #home.file."Pictures/.stignore".source = ./stignores/stignore-std;
+  #home.file."supernote/.stignore".source = ./stignores/stignore-std;
 
   systemd.user.tmpfiles.rules = [
     "d /home/l1npengtul/Downloads 0755 l1npengtul users -"
